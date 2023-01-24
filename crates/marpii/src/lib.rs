c839a9f8b9389a1957d157810eb0084dd95b8de0
Tendsin Mende <tendsin@protonmail.com>
2023-01-24 18:12:55 +0100

    send sync test for context and device doc OoS

--- crates/marpii/src/lib.rs
+++ crates/marpii/src/lib.rs
@@ -4,12 +4,12 @@
 //!
 //! # Usage
 //!
-//! In general marpii provides you with transparent wrappers around the main vulkan objects. This includes the [Instance](context::Instance), [Device](context::Device) and other lifetime sensitive structures. Those wrappers, if used, keep track of lifetimes
+//! In general MarpII provides you with transparent wrappers around the main Vulkan objects. This includes the [Instance](context::Instance), [Device](context::Device) and other lifetime sensitive structures. Those wrappers, if used, keep track of lifetimes
 //! and destruction of those objects when not needed anymore. Usually there are some helpers to simplify the creation of
 //! those. They can however also be created by hand.
 //!
 //!
-//! Structures that are not sensitive to lifetime requirements (like create infos) are not wrapped.
+//! Structures that are not sensitive to lifetime requirements (like create info) are not wrapped.
 #![deny(warnings)]
 #![feature(vec_into_raw_parts)]
 
@@ -20,6 +20,11 @@ pub use gpu_allocator;
 #[cfg(feature = "bytemuck")]
 pub use bytemuck;
 
+///Owned-Or-Shared wrapper. Allows us to implement generic over a type that might be owned or shared via [Arc](std::sync::Arc).
+///
+/// Note that you can convert from and into this type from Arcs and any value T.
+///
+/// If the context allows for the assumption that something is shared, a normal Arc should be preffered.
 pub use oos::OoS;
 
 ///Allocator related details. MarpII allows for custom allocators (usually the `A` parameter on the [Context](context::Ctx)).
@@ -32,7 +37,7 @@ pub mod context;
 ///Allocatable resources. Mostly [Image](resources::Image) and [Buffer](resources::Buffer).
 pub mod resources;
 
-///Window surface related stuctures. Includes a self managed [Surface](surface::Surface) type.
+///Window surface related structures. Includes a self managed [Surface](surface::Surface) type.
 pub mod surface;
 
 /// [Swapchain](swapchain::Swapchain) type that can be created from a [Surface](surface::Surface). Includes some helper function.
