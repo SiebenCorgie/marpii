7074ebc7b9eaa39f4a5c345840054caaf3a37e92
Tendsin Mende <tendsin@protonmail.com>
2023-06-06 18:59:29 +0200

    add custom_context example

---
+++ examples/custom_context/src/main.rs
@@ -0,0 +1,65 @@
+//! Example that shows how to configure a MarpII context at build-time to use a set of
+//! Vulkan features, iff they are present.
+//!
+//! Using that we can for instance configure advanced rendering features or compute features based
+//! on the hardware we are running on. A common example would be ray-tracing capabilities.
+
+use marpii::{self, ash::vk::PhysicalDeviceDynamicRenderingFeatures};
+
+fn main() {
+    simple_logger::SimpleLogger::new().init().unwrap();
+
+    let ev = winit::event_loop::EventLoop::new();
+    let window_handle = winit::window::Window::new(&ev).unwrap();
+
+    let ctx = marpii::context::Ctx::custom_context(Some(&window_handle), true, |mut builder| {
+        //The simplest thing we might want to check is if a extension is supported.
+        let dynamic = builder
+            .instance
+            .get_feature::<marpii::ash::vk::PhysicalDeviceDynamicRenderingFeatures>(
+                &builder.physical_device,
+            );
+        if dynamic.dynamic_rendering > 0 {
+            println!("Dynamic rendering is supported!");
+        } else {
+            println!("DynamicRendering not supported!");
+        }
+
+        //Bigger extensions might also declare custom limits, for instance all things related to accelleration structures.
+        let accel = builder
+            .instance
+            .get_feature::<marpii::ash::vk::PhysicalDeviceAccelerationStructureFeaturesKHR>(
+                &builder.physical_device,
+            );
+
+        if accel.acceleration_structure > 0 {
+            println!(
+                "Acceleration structure support with following properties:\n{:#?}",
+                accel
+            );
+        } else {
+            println!("AccelerationStructure not supported!");
+        }
+
+        //For faster query you can also just check the whole Vulkan 1.3 level
+        let vk13 = builder
+            .instance
+            .get_feature::<marpii::ash::vk::PhysicalDeviceVulkan13Features>(
+                &builder.physical_device,
+            );
+        println!("Vulkan 1.3 feature support: {:#?}", vk13);
+
+        //Finally depending on the querried results, you might want to push your feature set.
+        // In this case we enable just the dynamic rendering feature
+        if dynamic.dynamic_rendering > 0 {
+            builder = builder.with_feature(
+                PhysicalDeviceDynamicRenderingFeatures::builder()
+                    .dynamic_rendering(true)
+                    .build(),
+            );
+        }
+
+        builder
+    })
+    .unwrap();
+}
